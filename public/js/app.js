05b1152f08ff81f0d6dcdfeddf063452b198e639
Sam Delesque <sdelesque@domanistudios.com>
2015-05-12 11:56:39 -0400

    added bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,13 @@
 (function(){
 	FB.api(
-	    "/{page-id}/feed",
+	    "/1567614756859788/feed",
 	    function (response) {
 	      if (response && !response.error) {
 	        /* handle the result */
+	        console.log(response);
+	      }
+	      else{
+	      	console.error(response.error);
 	      }
 	    }
 	);
